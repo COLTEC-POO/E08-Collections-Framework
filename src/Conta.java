44b09710b5f6fb292d93b2c44ffb211147e6fff2
Raphael Menezes <raphael123ananias@hotmail.com>
2022-10-10 14:52:09 -0300

    Atualização para o uso de List<Operacao>
    Com a atualização, foi começado a alteração para utilizar o Colection.sort() porém ainda sem sucesso

---
+++ src/Conta.java
@@ -0,0 +1,230 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public abstract class Conta {
+
+    //atributos
+    public static Integer qtdContas = 0;
+
+    private Cliente cliente;
+    private Integer numConta;
+    private Double saldo;
+    private Double limite;
+    private Boolean tipo;
+    private ArrayList<Operacao> operacoes = new ArrayList<Operacao>();
+
+
+    // Construct para iniciar a conta
+
+    /**
+     * @param cliente
+     * @param tipo
+     * @param saldo_inicial
+     * @param limite
+     * @param numConta
+     */
+    public Conta(Cliente cliente, Boolean tipo, Double saldo_inicial, Double limite, Integer numConta){
+        try{
+            validaCliente(cliente);
+            validaTipo(tipo);
+            validaSaldo(saldo_inicial);
+            validaLimite(limite);
+            validaNumConta(numConta);
+            qtdContas++;
+        }catch (Exception e) {
+            System.out.println("Dados invalidos!");
+        }
+    }
+
+    public Conta(){
+        
+    }
+
+    //Getters
+    public Cliente cliente(){
+        return this.cliente;
+    }
+
+    public Boolean getTipo(){
+        return this.tipo;
+    }
+
+    public Double getSaldo(){
+        return this.saldo;
+    }
+
+    public Double getLimite(){
+        return this.limite;
+    }
+
+    public Integer getNumConta(){
+        return this.numConta;
+    }
+
+    public Double getMedia(){
+        return 1.0 * operacoes.size() / qtdContas;
+    }
+    public Integer getQtdOperacao(){
+        return this.operacoes.size();
+    }
+    //FIM Getters
+
+    //Setters
+    public void setCliente(Cliente cliente){
+        this.cliente = cliente;
+    }
+
+    public void setTipo(Boolean tipo){
+        this.tipo = tipo;
+    }
+
+    public void setSaldoInicial(Double saldo){
+        this.saldo = saldo;
+    }
+
+    public void setLimite(Double limite){
+        this.limite = limite;
+    }
+
+    public void setNumConta(Integer numConta){
+        this.numConta = numConta;
+    }
+    //FIM Setters
+
+    //Validadores de dados
+    public void validaCliente(Cliente cliente){
+        setCliente(cliente);
+    }
+
+    public void validaTipo(Boolean tipo){
+        setTipo(tipo);
+    }
+
+    public void validaSaldo(Double saldo){
+        if (saldo >= 0)
+            setSaldoInicial(saldo);
+    }
+
+    public void validaLimite(Double limite){
+        if (limite >= 0)
+            setLimite(0.0);
+    }
+
+    public void validaNumConta(Integer numConta){
+        setNumConta(numConta);
+    }
+    //FIM Validadores
+
+
+    //Operações com as contas
+    public void imprimirSaldo() {
+        System.out.println("Conta " + this.numConta);
+        System.out.println("Saldo: R$" + this.saldo);
+    };
+
+    public Boolean sacar(Double valor) {
+      if (valor > saldo || valor < 0) {
+          return false;
+      } else {
+          this.saldo-= valor;
+          System.out.println("Valor sacado: R$" + valor);
+          System.out.println("Novo Saldo: R$" + this.saldo);
+          operacoes.add(new OperacaoSaque(valor)); 
+          return true;
+      }
+    }
+
+    public Boolean depositar(Double valor) {
+        if (valor < 0){
+            return false;
+        } else {
+            this.saldo += valor;
+            System.out.println("Valor depositado: R$" + valor);
+            System.out.println("Novo Saldo: R$" + this.saldo);
+            operacoes.add(new OperacaoDeposito(valor));
+            return true;
+        }
+    }
+
+    public Boolean transferir(Conta destino, Double valor) {
+        if (this.sacar(valor)) {
+            destino.depositar(valor);
+            return true;
+        } else {
+            System.out.println("Transferencia incompleta, saldo disponivel: R$" + this.saldo);
+            return false;
+        }
+    }
+
+    public void mostraConta(){
+        System.out.println("Conta " + this.numConta);
+        System.out.println("Proprietário " + this.cliente.getNome());
+        System.out.println("Saldo: R$" + this.saldo + " Limite: R$ " + this.limite);
+    }
+
+    public void mostraConta(Boolean compelta){
+        System.out.println("Conta " + this.numConta);
+        System.out.println("Saldo: R$" + this.saldo + " Limite: R$ " + this.limite);
+        cliente.mostraCliente();
+    }
+
+    public void mostraExtrato(){
+        System.out.println("Conta " + this.numConta + " Titular: " + this.cliente.getNome() + " Saldo: R$ " + this.saldo);
+        if (operacoes.size() != 0){
+            for (Operacao o : operacoes){
+                if (o!= null){
+                    o.extrato();
+                }
+            }
+            System.out.println("Quantidade de Operações: " + this.operacoes.size());
+        }
+        else{
+            System.out.println("Não há operações");
+        }
+    }
+
+    public void mostraExtrato(Integer separado){
+        System.out.println("Conta " + this.numConta + " Titular: " + this.cliente.getNome() + " Saldo: R$ " + this.saldo);
+        if (operacoes.size() != 0){
+            ArrayList<Operacao> saques = Collections.sort(operacoes.compareTo(operacoes));
+            ArrayList<Operacao> depositos = operacoes.sort();
+            for (Operacao o : depositos){
+                if (o!= null){
+                    o.extrato();
+                }
+            }
+            System.out.println("Depositos: " + this.depositos.size());
+
+            for (Operacao o : saques){
+                if (o!= null){
+                    o.extrato();
+                }
+            }
+            System.out.println("Depositos: " + this.saques.size());
+
+            System.out.println("Quantidade de Operações: " + this.operacoes.size());
+        }
+        else{
+            System.out.println("Não há operações");
+        }
+    }
+
+    public abstract Double calculaTaxas();
+
+    public void imprimeExtratoTaxas(){
+        System.out.println("\n=== Extrato de Taxas ===");
+
+        System.out.println("\nManutenção de Conta: R$" + this.calculaTaxas());
+
+        System.out.println("\n\nOperações");
+        for (ITaxas operacao : operacoes) {
+            if (operacao.calculaTaxas() != 0.0)
+                System.out.println("Saque: " + operacao.calculaTaxas());
+        }
+
+
+    }
+    //FIM Operações com as contas
+
+}
